10bb781739c896f4c2d177a0399b22131ea94834
matiasgasparg <matiasgasparg@gmail.com>
2022-08-22 22:11:54 -0300

    Agregado Carousel con imagenes + lupa del buscador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
+
+})
+export class CarouselComponent {
+  images = ["qF1x8nc", "p12y9YS", "p12y9YS"].map((n) => `https://i.ibb.co/${n}/zcappeimagencarousel.jpg`);
+
+
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+  }
+}
